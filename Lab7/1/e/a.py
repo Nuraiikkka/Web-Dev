b52805a5e338c4e46726c301dab11a90dcbbf0aa
Nuraiikkka <aitbazarnurai@gmail.com>
2025-03-13 12:44:32 +0500

    lab7

---
+++ Lab7/1/e/a.py
@@ -0,0 +1,5 @@
+def min_num(a, b, c, d):
+    return min(a, b, c,d)
+
+a, b, c, d = map(int, input().split())
+print(min_num(a, b, c, d))
---
+++ Lab7/1/e/b.py
@@ -0,0 +1,4 @@
+def pow_er(a, b):
+    return (pow(a, b))
+a, b = map(int, input().split())
+print(pow_er(a, b))
